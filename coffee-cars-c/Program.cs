d7ab5c4e66f15008cf07dab3bdb00a63b830fd25
fernando5514 <fernando.ramirez.f55@gmail.com>
2022-04-24 21:00:50 +0200

    0.8
    c# terminado

--- coffee-cars-c/Program.cs
+++ coffee-cars-c/Program.cs
@@ -5,16 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using System.Security.Cryptography;
+using System.IO;
 
 namespace coffee_cars_c
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-
-            string connectionString = "datasource=83.47.104.187;port=3306;username=admin;password=admin;database=coffee_cars";
-
+            conex conex = new conex();//creo objeto conex para acceder a la conexión a la base de datos
+            string connectionString = conex.GetConnection();
             string query = @"
                 SELECT * FROM `coffee_cars`.usuarios;
             
@@ -23,37 +23,9 @@ namespace coffee_cars_c
 
             MySqlConnection connection = new MySqlConnection(connectionString);
             MySqlCommand command = new MySqlCommand(query, connection);
-            MySqlDataReader reader;
-
-            try
-            {
-                connection.Open();
-
-                reader = command.ExecuteReader();
-                if (reader.HasRows)
-                {
- 
-                    while(reader.Read())
-                    {
-                        if (reader.GetString(0) == "1")
-                        {
-                            Console.WriteLine("hola");
-                        }
-                    }
-
-                }
-                else
-                {
-                    Console.WriteLine("Error al leer");
-                }
-                connection.Close();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
 
             int menu = 0;
+            Console.WriteLine("\t\t BIENVENIDO A COFFEE CARS");
             do
             {
                 Console.WriteLine("\nIntroduzca la opción deseada:\n1.Registrarse\n2.Logearse\n3.Salir de la aplicación");
@@ -72,7 +44,7 @@ namespace coffee_cars_c
                     
                     do
                     {
-                        Console.WriteLine("Introduzca su email");
+                        Console.WriteLine("Introduzca su nombre de usuario");
                         usuario = Convert.ToString(Console.ReadLine());
                         //accedemos a la base de datos
                         aux=Comprobarlog(query,connection,command,usuario);
@@ -81,7 +53,7 @@ namespace coffee_cars_c
                             Console.WriteLine("Usuario ya registrado");
                         }
                     } while (aux != 0);
-                    Console.WriteLine("Vuelva a introducir su email");
+                    Console.WriteLine("Vuelva a introducir su nombre de usuario");
                     usuario2 = Convert.ToString(Console.ReadLine());
 
                     //limpiamos el buffer de entrada
@@ -103,7 +75,7 @@ namespace coffee_cars_c
                         apellidos = Convert.ToString(Console.ReadLine());
 
                         aux = 0;
-
+                        contraseña = seguridad.Encrypt(usuario,contraseña,256);
                         aux = Registro(query, connection, command, usuario, contraseña, nombre, apellidos);
                         if (aux == 1)
                         {
@@ -121,12 +93,40 @@ namespace coffee_cars_c
                 }
                 if (menu == 2)
                 {
-                    string palabra;
-                    palabra = RandomString();
-                    Console.WriteLine(palabra);
-                }
+                    int aux = 0;
+                    string usuario, contraseña,clave;
+                    Console.WriteLine("Introduzca su nombre de usuario");
+                    usuario = Convert.ToString(Console.ReadLine());
+                    //accedemos a la base de datos
+                    aux = Comprobarlog(query, connection, command, usuario);
+                    if (aux == 0)
+                    {
+                        Console.WriteLine("El usuario no exixte");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Introduzca su contraseña:");
+                        contraseña = Convert.ToString(Console.ReadLine());
+                        contraseña = seguridad.Encrypt(usuario, contraseña, 256);
+                        aux=ComprobarContraseña(query, connection, command, usuario,contraseña);
+                        if (aux == 1)
+                        {
+                            clave = Exito(query, connection, command, usuario, contraseña);
+                            Console.WriteLine("\n\t\tCOFFEE CARS");
+                            Console.WriteLine("\n\tBienvenido "+usuario+" !!!");
+                            Console.WriteLine("\n\tSu clave es: "+clave);
+                            Console.WriteLine("\n\tTiene acceso a http://coffee-cars.duckdns.org/template_html_startup/login.php ");
+                        }
+                        else
+                        {
+                            Console.WriteLine("ERROR ----> Contraseña incorrecta");
+                        }
+                    }
 
+
+                }
             } while (menu != 3);
+
         }
 
 
@@ -152,6 +152,55 @@ namespace coffee_cars_c
             connection.Close();
             return 0;
         }
+
+        public static int ComprobarContraseña(string query, MySqlConnection connection, MySqlCommand command, string usuario,string contraseña)
+        {
+            //damos al query de búsqueda en la base de datos y la abrimos para hacer la búsqueda
+            query = @"SELECT usuario,contrasena FROM `coffee_cars`.usuarios;";
+            command = new MySqlCommand(query, connection);
+            MySqlDataReader reader;
+            connection.Open();
+            reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    if (reader.GetString(0) == usuario&&reader.GetString(1)==contraseña)
+                    {
+                        connection.Close();
+                        return 1;
+                    }
+                }
+            }
+            connection.Close();
+            return 0;
+        }
+        public static string Exito(string query, MySqlConnection connection, MySqlCommand command, string usuario, string contraseña)
+        {
+            string clave="";
+            //damos al query de búsqueda en la base de datos y la abrimos para hacer la búsqueda
+            query = @"SELECT usuario,contrasena,claveuser FROM `coffee_cars`.usuarios;";
+            command = new MySqlCommand(query, connection);
+            MySqlDataReader reader;
+            connection.Open();
+            reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    if (reader.GetString(0) == usuario && reader.GetString(1) == contraseña)
+                    {
+                        clave=Convert.ToString(reader.GetString(2));
+                        connection.Close();
+                        return clave;
+                    }
+                }
+            }
+            connection.Close();
+            return clave;
+        }
+
+
         public static int Registro(string query, MySqlConnection connection, MySqlCommand command, string usuario,string contraseña,string nombre, string apellidos)
         {
             string clave= RandomString();
@@ -171,7 +220,7 @@ namespace coffee_cars_c
         public static string RandomString()
         {
             const string src = "abcdefghijklmnopqrstuvwxyz0123456789";
-            int length = 16;
+            int length = 10;
             var sb = new StringBuilder();
             Random RNG = new Random();
             for (var i = 0; i < length; i++)
@@ -181,6 +230,7 @@ namespace coffee_cars_c
             }
             return sb.ToString();
         }
+
     }
 
 }
---
+++ coffee-cars-c/conex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace coffee_cars_c
+{
+    class conex
+    {
+        protected String connectionString = "datasource=83.47.104.187;port=3306;username=admin;password=admin;database=coffee_cars";
+
+        public String GetConnection()
+        {
+            return connectionString;
+        }
+    }
+}
